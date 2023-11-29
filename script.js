0afb1748ad0f92eb398c52f96ea1a1b0a53b4122
jessi994 <jessied200@yahoo.co.uk>
2023-11-29 00:47:39 +0000

    Updates made to generatePassword and getRandom functions

--- script.js
+++ script.js
@@ -90,6 +90,7 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // Initialise object containing password options
   let passwordCriteria = {
     pass_length: 0,
     lowercase: false,
@@ -97,16 +98,18 @@ function getPasswordOptions() {
     numbers: false,
     special_char: false,
   };
+  // If password length is out of bounds keep running until user enters an inbounds number
   while(true){
     passwordCriteria.pass_length = prompt("How long do you want your password to be?");
     if (passwordCriteria.pass_length < 8 || passwordCriteria.pass_length > 128 ) {
       alert('Out of bounds! Please enter a password length between 8 and 128')
     }
+    // Once user has entered an accepted password length exit the while loop
     else{
       break;
     }
-
   }
+  // Set the rest of the password options given by the user
   passwordCriteria.lowercase = confirm("Would you like Lowercase characters in your password?")
   passwordCriteria.uppercase = confirm("Would you like Uppercase characters in your password?")
   passwordCriteria.numbers = confirm("Would you like numbers in your password?")
@@ -114,16 +117,38 @@ function getPasswordOptions() {
   
   return passwordCriteria;
 }
-let results = getPasswordOptions()
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+  return arr[Math.floor(Math.random()*arr.length)]
 
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  let options = getPasswordOptions()
+  var all_char = []
+  if (options.lowercase){
+    all_char += lowerCasedCharacters
+  }
+  if (options.uppercase){
+    all_char += upperCasedCharacters
+  }
+  if (options.numbers){
+    all_char += numericCharacters
+  }
+  if (options.special_char){
+    all_char += specialCharacters
+  }
+  
+  let password = []
+  while(password.length < options.pass_length){
+    const char = all_char[Math.floor(Math.random()*all_char.length)];
+    password.push(char)
+  }
 
+  return password
 }
 
 // Get references to the #generate element
